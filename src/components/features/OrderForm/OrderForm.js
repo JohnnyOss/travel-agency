61a3cfda59d462dec0a1f3887eb031598fbd4460
Jan Ossolinski <ossolinski.jan@gmail.com>
2021-01-29 17:59:37 +0000

    Add API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,8 +4,39 @@ import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import OrderOption from '../OrderOption/OrderOption.js';
 import pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
 
 import {Grid, Row, Col} from 'react-flexbox-grid';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
 
@@ -28,6 +59,7 @@ class OrderForm extends React.Component {
           )}
           <Col xs={12}>
             <OrderSummary tripCost={tripCost} options={options} />
+            <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
           </Col>
         </Row>
       </Grid>
