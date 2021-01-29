6a90c77cea6ac67197850619c5a33e40215da670
Jan Ossolinski <ossolinski.jan@gmail.com>
2021-01-29 15:35:30 +0000

    Fixed OrderOptionDate.js & Add OrderOption.test.js

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
 
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const OrderOptionDate = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  return (
-    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-  );
+const OrderOptionDate = ({setOptionValue, currentValue}) => (
+  <DatePicker selected={currentValue} onChange = {setOptionValue} />
+);
+
+OrderOptionDate.propTypes = {
+  currentValue: PropTypes.any,
+  setOptionValue:PropTypes.func,
 };
 
 export default OrderOptionDate;
